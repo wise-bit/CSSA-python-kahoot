53794557d31a2dd3774df22979c03af79b72f375
Satrajit Chatterjee <satrajit2016@gmail.com>
2020-11-16 00:41:28 -0500

    updated to more efficient solution

--- Solutions/Q6LastWordLength.py
+++ Solutions/Q6LastWordLength.py
@@ -1,9 +1,15 @@
 
 def last_word_length(s):
-	words = s.split()
-	if len(words) == 0:
-		return 0
-	return len(words[-1])
+	s = s.strip() 
+	length = 0
+  
+	for x in s: 
+		if x == " ": 
+			length = 0
+		else: 
+			length += 1
+
+	return length
 
 
 sol = last_word_length("python is fun")
